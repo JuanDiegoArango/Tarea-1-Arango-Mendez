c79a9244dbf097ab9c88868c0cc5a7946a6a1149
MendezV <som.felipe66@gmail.com>
2016-02-29 06:19:58 -0500

    todos los plots menos energía

--- Problema de los tres cuerpos/plot2zoom2.py
+++ Problema de los tres cuerpos/plot2zoom2.py
@@ -24,16 +24,16 @@ figura = plt.figure(figsize=(12.0, 17.0))
 
 
 ax = figura.add_subplot(2, 1, 1)
-plt.ylim([-0.65,0.65])
-plt.xlim([-2.4, -0.8])
+plt.ylim([-0.4,0.4])
+plt.xlim([-0.2, 0.7])
 plt.scatter(q3S, p3S,   s=1, facecolor='0.5', lw = 0)
 ax.set_title("$\mathrm{Mapa}$"+" "+"$\mathrm{de}$"+" "+"$\mathrm{Poincare}$"+" "+"$\mathrm{Simplectico}$")
 ax.set_xlabel("$q3$")
 ax.set_ylabel("$p3$")
 
 ax = figura.add_subplot(2, 1, 2)
-plt.ylim([-1, 1.5])
-plt.xlim([-2.8, 0.8])
+plt.ylim([-0.4,0.4])
+plt.xlim([-0.2, 0.7])
 plt.scatter(q3RK, p3RK,   s=1, facecolor='0.5', lw = 0)
 ax.set_title("$\mathrm{Mapa}$"+" "+"$\mathrm{de}$"+" "+"$\mathrm{Poincare}$"+" "+"$\mathrm{RK4}$")
 ax.set_xlabel("$q3$")
@@ -43,5 +43,6 @@ t = t[:-4]
 a = a[:-4]
 
 plt.savefig(t+"_"+a+".pdf")
+
 
 
